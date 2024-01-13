46c7670272a67079cf2e788278a15dc07b9aa015
unknown <shahbaz_ali@outlook.in>
2024-01-13 18:28:32 +0530

    use nestjs config

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
+    constructor(config: ConfigService) {
         super({
             datasources: {
                 db: {
-                    url: 'postgresql://shahbaz42:123456789@localhost:5434/nest?schema=public'
+                    url: config.get('DATABASE_URL')
                 }
             }
         })
