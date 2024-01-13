204ed4481d2a0881a70c39db1e61e9c760dda214
unknown <shahbaz_ali@outlook.in>
2024-01-13 09:19:59 +0530

    add prisma migration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@nestjs/common";
+// import { User, Bookmark } from '@prisma/client'
 
 @Injectable({})
 export class AuthService{
     signin(){
         return {
+            
             msg : "I have  signed in."
         }
     }
