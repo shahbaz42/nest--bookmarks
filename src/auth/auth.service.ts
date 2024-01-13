8d66eeba85f432f2ab435d066ffa6e09eaf472c7
unknown <shahbaz_ali@outlook.in>
2024-01-13 11:41:56 +0530

    add validations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 // import { User, Bookmark } from '@prisma/client'
 
 @Injectable({})
 export class AuthService{
+    constructor(private prisma: PrismaService){}
     signin(){
         return {
-            
             msg : "I have  signed in."
         }
     }
